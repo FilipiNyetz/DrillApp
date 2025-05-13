54012d61d47da02c3a7ede8c1eadf03f01da8dd0
FilipiNyetz <romao.martins.filipi@gmail.com>
2025-05-13 15:20:24 -0300

    Initial Commit

---
+++ Drill/DrillApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrillApp.swift
+//  Drill
+//
+//  Created by Filipi Romão on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct DrillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrillTests/DrillTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrillTests.swift
+//  DrillTests
+//
+//  Created by Filipi Romão on 13/05/25.
+//
+
+import Testing
+@testable import Drill
+
+struct DrillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
