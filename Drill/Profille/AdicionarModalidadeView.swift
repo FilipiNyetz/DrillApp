9198cfdbe3f722c4062756720d3069472678467b
FilipiNyetz <romao.martins.filipi@gmail.com>
2025-05-28 16:54:55 -0300

    Addition of multiple modalities, changed modalities

---
+++ Drill/Profille/AdicionarModalidadeView.swift
@@ -0,0 +1,18 @@
+//
+//  AdicionarModalidadeView.swift
+//  Drill
+//
+//  Created by Filipi Romão on 27/05/25.
+//
+
+import SwiftUI
+
+struct AdicionarModalidadeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdicionarModalidadeView()
+}
---
+++ Drill/Profille/PersonalizarModalidade.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalizarModalidade.swift
+//  Drill
+//
+//  Created by Filipi Romão on 27/05/25.
+//
+
+import SwiftUI
+
+struct PersonalizarModalidade: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonalizarModalidade()
+}
