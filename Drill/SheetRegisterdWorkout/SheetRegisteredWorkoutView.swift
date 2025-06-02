0fd195440680a1cd3bf5aa9649ab59b2245efe58
FilipiNyetz <romao.martins.filipi@gmail.com>
2025-06-02 12:01:34 -0300

    Fixed registered workout in same day, change layout calendar

---
+++ Drill/SheetRegisterdWorkout/SheetRegisteredWorkoutView.swift
@@ -0,0 +1,18 @@
+//
+//  SheetRegisteredWorkoutView.swift
+//  Drill
+//
+//  Created by Filipi Romão on 02/06/25.
+//
+
+import SwiftUI
+
+struct SheetRegisteredWorkoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetRegisteredWorkoutView()
+}
